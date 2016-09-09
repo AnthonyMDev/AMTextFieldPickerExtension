7e6596b0f199f552f64edb638b83f47471522b6c
Jett Farmer <jett@app-order.com>
2016-09-09 14:03:50 -0700

    Updated indentation. Minor formatting changes to improve readability.

--- AMTextFieldPickerExtension/UITextField+AMSetPicker.swift
+++ AMTextFieldPickerExtension/UITextField+AMSetPicker.swift
@@ -9,101 +9,105 @@
 import UIKit
 
 private enum AssociatedObjectKeys: String {
-  case RowTitles = "AMTextFieldPicker_rowTitles"
+    case RowTitles = "AMTextFieldPicker_rowTitles"
 }
 
 /**
-*  This extension to `UITextField` allows you to set a `UIPickerView` to the `pickerView` property to set the given `UIPickerView` as the `inputView` for the `UITextField`
-
-  :discussion: Setting the `pickerView` for the text field will also set up a `UIToolbar` with a "Done" button as the text field's `inputAccessoryView`. When pressed, the "Done" button will set the text field's text from the `pickerView`'s `delegate`.
-*/
+ *  This extension to `UITextField` allows you to set a `UIPickerView` to the `pickerView` property to set the given `UIPickerView` as the `inputView` for the `UITextField`
+ 
+ :discussion: Setting the `pickerView` for the text field will also set up a `UIToolbar` with a "Done" button as the text field's `inputAccessoryView`. When pressed, the "Done" button will set the text field's text from the `pickerView`'s `delegate`.
+ */
 public extension UITextField {
-  
-  /// The `UIPickerView` for the text field. Set this to configure the `inputView` and `inputAccessoryView` for the text field.
-  public var pickerView: UIPickerView? {
-    get {
-      return self.inputView as? UIPickerView
-    }
-    set {
-      setInputViewToPicker(newValue)
-    }
-  }
-  
-  /// The `UIDatePicker` for the text field. Set this to configure the `inputView` and `inputAccessoryView` for the text field.
-  public var datePicker: UIDatePicker? {
-    get {
-      return self.inputView as? UIDatePicker
-    }
-    set {
-      setInputViewToPicker(newValue)
-    }
-  }
-  
-  fileprivate func setInputViewToPicker(_ picker: UIView?) {
-    self.inputView = picker
-    self.inputAccessoryView = picker != nil ? pickerToolbar() : nil
-  }
-  
-  fileprivate func pickerToolbar() -> UIToolbar {
-    let toolbar = UIToolbar(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 320, height: 44)))
-    let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(UITextField.didPressPickerDoneButton(_:)))
-    toolbar.items = [flexibleSpace, doneButton]
     
-    return toolbar
-  }
-  
-  /// The formatting string to use to set the text field's `text` when using the `datePicker`.
-  /// See `NSDateFormatter` for more information
-  /// Defaults to "M/d/yy"
-  public var dateFormat: String {
-    get {
-      return objc_getAssociatedObject(self, &AssociatedKeys.DateFormat) as? String ?? "M/d/yy"
+    /// The `UIPickerView` for the text field. Set this to configure the `inputView` and `inputAccessoryView` for the text field.
+    public var pickerView: UIPickerView? {
+        get {
+            return self.inputView as? UIPickerView
+        }
+        set {
+            setInputViewToPicker(newValue)
+        }
     }
-    set {
-      objc_setAssociatedObject(self, &AssociatedKeys.DateFormat, newValue as NSString, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-    }
-  }
-  
-  fileprivate struct AssociatedKeys {
-    static var DateFormat = "am_DateFormat"
-  }
-  
-  /**
-  This method is called when the "Done" button on the `inputAccessoryView` toolbar is pressed.
-  
-  :discussion: This method will set the text field's text with the title for the selected row in the `pickerView` in component `0` from the `pickerView`'s `delegate.
-  
-  - parameter sender: The "Done" button sending the action.
-  */
-  public func didPressPickerDoneButton(_ sender: AnyObject) {
-    guard pickerView != nil || datePicker != nil else { return }
     
-    if pickerView != nil {
-      setTextFromPickerView()
-      
-    } else if datePicker != nil {
-      setTextFromDatePicker()
+    /// The `UIDatePicker` for the text field. Set this to configure the `inputView` and `inputAccessoryView` for the text field.
+    public var datePicker: UIDatePicker? {
+        get {
+            return self.inputView as? UIDatePicker
+        }
+        set {
+            setInputViewToPicker(newValue)
+        }
     }
-    DispatchQueue.main.async(execute: { () -> Void in
-      self.sendActions(for: .editingChanged)
-    })
-    resignFirstResponder()
-  }
-  
-  fileprivate func setTextFromPickerView() {
-    if let selectedRow = pickerView?.selectedRow(inComponent: 0),
-      let title = pickerView?.delegate?.pickerView?(pickerView!, titleForRow: selectedRow, forComponent: 0) {
-        self.text = title
+    
+    fileprivate func setInputViewToPicker(_ picker: UIView?) {
+        self.inputView = picker
+        self.inputAccessoryView = picker != nil ? pickerToolbar() : nil
     }
-  }
-  
-  fileprivate func setTextFromDatePicker() {
-    if let date = datePicker?.date {
-      let formatter = DateFormatter()
-      formatter.dateFormat = dateFormat
-      self.text = formatter.string(from: date)
+    
+    fileprivate func pickerToolbar() -> UIToolbar {
+        let toolbar = UIToolbar(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 320, height: 44)))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done,
+                                         target: self,
+                                         action: #selector(UITextField.didPressPickerDoneButton(_:)))
+        
+        toolbar.items = [flexibleSpace, doneButton]
+        
+        return toolbar
     }
-  }
-  
+    
+    /// The formatting string to use to set the text field's `text` when using the `datePicker`.
+    /// See `NSDateFormatter` for more information
+    /// Defaults to "M/d/yy"
+    public var dateFormat: String {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.DateFormat) as? String ?? "M/d/yy"
+        }
+        set {
+            objc_setAssociatedObject(self,
+                                     &AssociatedKeys.DateFormat, newValue as NSString, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    fileprivate struct AssociatedKeys {
+        static var DateFormat = "am_DateFormat"
+    }
+    
+    /**
+     This method is called when the "Done" button on the `inputAccessoryView` toolbar is pressed.
+     
+     :discussion: This method will set the text field's text with the title for the selected row in the `pickerView` in component `0` from the `pickerView`'s `delegate.
+     
+     - parameter sender: The "Done" button sending the action.
+     */
+    public func didPressPickerDoneButton(_ sender: AnyObject) {
+        guard pickerView != nil || datePicker != nil else { return }
+        
+        if pickerView != nil {
+            setTextFromPickerView()
+            
+        } else if datePicker != nil {
+            setTextFromDatePicker()
+        }
+        DispatchQueue.main.async(execute: { () -> Void in
+            self.sendActions(for: .editingChanged)
+        })
+        resignFirstResponder()
+    }
+    
+    fileprivate func setTextFromPickerView() {
+        if let selectedRow = pickerView?.selectedRow(inComponent: 0),
+            let title = pickerView?.delegate?.pickerView?(pickerView!, titleForRow: selectedRow, forComponent: 0) {
+            self.text = title
+        }
+    }
+    
+    fileprivate func setTextFromDatePicker() {
+        if let date = datePicker?.date {
+            let formatter = DateFormatter()
+            formatter.dateFormat = dateFormat
+            self.text = formatter.string(from: date)
+        }
+    }
+    
 }
--- AMTextFieldPickerExtensionTests/AMTextFieldPickerExtensionTests.swift
+++ AMTextFieldPickerExtensionTests/AMTextFieldPickerExtensionTests.swift
@@ -13,199 +13,199 @@ import Nimble
 import AMTextFieldPickerExtension
 
 class AMTextFieldPickerExtensionTests: XCTestCase {
-  
-  var sut: UITextField!
-  
-  /**
-  *  MARK: - Test Set Up
-  */
-  
-  override func setUp() {
-    super.setUp()
     
-    sut = UITextField()
-  }
-  
-  /**
-  *  MARK: Tests
-  */
-  
-  func test__setPickerView__sets_inputViewToPicker() {
-    // given
-    let expected = UIPickerView()
+    var sut: UITextField!
     
-    // when
-    sut.pickerView = expected
+    /**
+     *  MARK: - Test Set Up
+     */
     
-    // then
-    expect(self.sut.inputView).to(beIdenticalTo(expected))
-  }
-  
-  func test__setPickerView__setsUpToolbar() {
-    // when
-    sut.pickerView = UIPickerView()
-    
-    // then
-    let toolbar = self.sut.inputAccessoryView as? UIToolbar
-    expect(toolbar).toNot(beNil())
-    expect(toolbar?.frame.height).to(beGreaterThanOrEqualTo(44))
-    expect(toolbar?.items?.count).to(equal(2))
-    let doneButton = toolbar?.items?.last
-    expect(doneButton?.action).to(equal(#selector(UITextField.didPressPickerDoneButton(_:))))
-    expect(doneButton?.target as? UITextField).to(beIdenticalTo(self.sut))
-  }
-  
-  func test__setPickerView__givenNil_setsInputAccessoryViewNil() {
-    // given
-    sut.inputAccessoryView = UIView()
-    
-    // when
-    sut.pickerView = nil
-    
-    // then
-    expect(self.sut.inputAccessoryView).to(beNil())
-  }
-  
-  func test__setDatePicker__sets_inputViewToDatePicker() {
-    // given
-    let expected = UIDatePicker()
-    
-    // when
-    sut.datePicker = expected
-    
-    // then
-    expect(self.sut.inputView).to(beIdenticalTo(expected))
-  }
-  
-  func test__setDatePicker__setsUpToolbar() {
-    // when
-    sut.datePicker = UIDatePicker()
-    
-    // then
-    let toolbar = self.sut.inputAccessoryView as? UIToolbar
-    expect(toolbar).toNot(beNil())
-    expect(toolbar?.frame.height).to(beGreaterThanOrEqualTo(44))
-    expect(toolbar?.items?.count).to(equal(2))
-    let doneButton = toolbar?.items?.last
-    expect(doneButton?.action).to(equal(#selector(UITextField.didPressPickerDoneButton(_:))))
-    expect(doneButton?.target as? UITextField).to(beIdenticalTo(self.sut))
-  }
-  
-  func test__setDatePicker__givenNil_setsInputAccessoryViewNil() {
-    // given
-    sut.inputAccessoryView = UIView()
-    
-    // when
-    sut.datePicker = nil
-    
-    // then
-    expect(self.sut.inputAccessoryView).to(beNil())
-  }
-  
-  func test__didPressPickerDoneButton__givenDelegate_setsTextFieldText_toPickerViewSelectedRowText() {
-    // given
-    let expected = "Row Title Text"
-    
-    let mockPicker = MockPickerView()
-    let mockDelegate = MockPickerViewTitleDelegate()
-    mockDelegate.mockTitle = expected
-    mockPicker.delegate = mockDelegate
-    
-    sut.pickerView = mockPicker
-    
-    // when
-    sut.didPressPickerDoneButton(self)
-    
-    // then
-    expect(self.sut.text).to(equal(expected))
-  }
-  
-  func test__didPressPickerDoneButton__givenDelegate_sendsEditingChangedAction() {
-    // given
-    class MockSUT: UITextField {
-      var controlEventsSent: UIControlEvents?      
-      fileprivate override func sendActions(for controlEvents: UIControlEvents) {
-        controlEventsSent = controlEvents
-      }
+    override func setUp() {
+        super.setUp()
+        
+        sut = UITextField()
     }
     
-    let SUT = MockSUT()
-    let mockPicker = MockPickerView()
-    let mockDelegate = MockPickerViewTitleDelegate()
-    mockPicker.delegate = mockDelegate
-    SUT.pickerView = mockPicker
+    /**
+     *  MARK: Tests
+     */
     
-    // when
-    SUT.didPressPickerDoneButton(self)
+    func test__setPickerView__sets_inputViewToPicker() {
+        // given
+        let expected = UIPickerView()
+        
+        // when
+        sut.pickerView = expected
+        
+        // then
+        expect(self.sut.inputView).to(beIdenticalTo(expected))
+    }
     
-    // then
-    expect(SUT.controlEventsSent).toEventually(equal(UIControlEvents.editingChanged))
-  }
-  
-  func test__didPressPickerDoneButton__resignsFirstResponder() {
-    // given
-    let window = UIWindow()
-    window.addSubview(sut)
+    func test__setPickerView__setsUpToolbar() {
+        // when
+        sut.pickerView = UIPickerView()
+        
+        // then
+        let toolbar = self.sut.inputAccessoryView as? UIToolbar
+        expect(toolbar).toNot(beNil())
+        expect(toolbar?.frame.height).to(beGreaterThanOrEqualTo(44))
+        expect(toolbar?.items?.count).to(equal(2))
+        let doneButton = toolbar?.items?.last
+        expect(doneButton?.action).to(equal(#selector(UITextField.didPressPickerDoneButton(_:))))
+        expect(doneButton?.target as? UITextField).to(beIdenticalTo(self.sut))
+    }
     
-    sut.pickerView = UIPickerView()
+    func test__setPickerView__givenNil_setsInputAccessoryViewNil() {
+        // given
+        sut.inputAccessoryView = UIView()
+        
+        // when
+        sut.pickerView = nil
+        
+        // then
+        expect(self.sut.inputAccessoryView).to(beNil())
+    }
     
-    sut.becomeFirstResponder()
+    func test__setDatePicker__sets_inputViewToDatePicker() {
+        // given
+        let expected = UIDatePicker()
+        
+        // when
+        sut.datePicker = expected
+        
+        // then
+        expect(self.sut.inputView).to(beIdenticalTo(expected))
+    }
     
-    // when
-    sut.didPressPickerDoneButton(self)
+    func test__setDatePicker__setsUpToolbar() {
+        // when
+        sut.datePicker = UIDatePicker()
+        
+        // then
+        let toolbar = self.sut.inputAccessoryView as? UIToolbar
+        expect(toolbar).toNot(beNil())
+        expect(toolbar?.frame.height).to(beGreaterThanOrEqualTo(44))
+        expect(toolbar?.items?.count).to(equal(2))
+        let doneButton = toolbar?.items?.last
+        expect(doneButton?.action).to(equal(#selector(UITextField.didPressPickerDoneButton(_:))))
+        expect(doneButton?.target as? UITextField).to(beIdenticalTo(self.sut))
+    }
     
-    // then
-    expect(self.sut.isFirstResponder).to(beFalse())
-  }
-  
-  func test__didPressPickerDoneButton__givenDatePicker__setsTextFieldText_withDateFormat() {
-    // given
-    let format = "MM/d/yyyy"
-    sut.dateFormat = format
+    func test__setDatePicker__givenNil_setsInputAccessoryViewNil() {
+        // given
+        sut.inputAccessoryView = UIView()
+        
+        // when
+        sut.datePicker = nil
+        
+        // then
+        expect(self.sut.inputAccessoryView).to(beNil())
+    }
     
-    sut.datePicker = UIDatePicker()
-    let date = Date()
-    sut.datePicker?.setDate(date, animated: false)
+    func test__didPressPickerDoneButton__givenDelegate_setsTextFieldText_toPickerViewSelectedRowText() {
+        // given
+        let expected = "Row Title Text"
+        
+        let mockPicker = MockPickerView()
+        let mockDelegate = MockPickerViewTitleDelegate()
+        mockDelegate.mockTitle = expected
+        mockPicker.delegate = mockDelegate
+        
+        sut.pickerView = mockPicker
+        
+        // when
+        sut.didPressPickerDoneButton(self)
+        
+        // then
+        expect(self.sut.text).to(equal(expected))
+    }
     
-    let formatter = DateFormatter()
-    formatter.dateFormat = format
-    let expected = formatter.string(from: date)
+    func test__didPressPickerDoneButton__givenDelegate_sendsEditingChangedAction() {
+        // given
+        class MockSUT: UITextField {
+            var controlEventsSent: UIControlEvents?
+            fileprivate override func sendActions(for controlEvents: UIControlEvents) {
+                controlEventsSent = controlEvents
+            }
+        }
+        
+        let SUT = MockSUT()
+        let mockPicker = MockPickerView()
+        let mockDelegate = MockPickerViewTitleDelegate()
+        mockPicker.delegate = mockDelegate
+        SUT.pickerView = mockPicker
+        
+        // when
+        SUT.didPressPickerDoneButton(self)
+        
+        // then
+        expect(SUT.controlEventsSent).toEventually(equal(UIControlEvents.editingChanged))
+    }
     
-    // when
-    sut.didPressPickerDoneButton(self)
+    func test__didPressPickerDoneButton__resignsFirstResponder() {
+        // given
+        let window = UIWindow()
+        window.addSubview(sut)
+        
+        sut.pickerView = UIPickerView()
+        
+        sut.becomeFirstResponder()
+        
+        // when
+        sut.didPressPickerDoneButton(self)
+        
+        // then
+        expect(self.sut.isFirstResponder).to(beFalse())
+    }
     
-    // then
-    expect(self.sut.text).to(equal(expected))
-  }
-  
+    func test__didPressPickerDoneButton__givenDatePicker__setsTextFieldText_withDateFormat() {
+        // given
+        let format = "MM/d/yyyy"
+        sut.dateFormat = format
+        
+        sut.datePicker = UIDatePicker()
+        let date = Date()
+        sut.datePicker?.setDate(date, animated: false)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        let expected = formatter.string(from: date)
+        
+        // when
+        sut.didPressPickerDoneButton(self)
+        
+        // then
+        expect(self.sut.text).to(equal(expected))
+    }
+    
 }
 
 class MockPickerView: UIPickerView {
-  
-  var mockSelectedRow: Int = 0
-  
-  override func selectedRow(inComponent component: Int) -> Int {
-    return mockSelectedRow
-  }
-  
+    
+    var mockSelectedRow: Int = 0
+    
+    override func selectedRow(inComponent component: Int) -> Int {
+        return mockSelectedRow
+    }
+    
 }
 
 class MockPickerViewTitleDelegate: NSObject, UIPickerViewDelegate {
-  
-  var mockTitle: String = ""
-  
-  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-    return mockTitle
-  }
-  
+    
+    var mockTitle: String = ""
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return mockTitle
+    }
+    
 }
 
 class TextFieldEventObserver {
-  
-  var editingChanged = false
-  
-  func editingDidChange() {
-    editingChanged = true
-  }
-  
+    
+    var editingChanged = false
+    
+    func editingDidChange() {
+        editingChanged = true
+    }
+    
 }
